b794277efaf617ec19f1910143880c6aecc64d0f
bckspce <49206472+pwamoreno@users.noreply.github.com>
2025-07-01 13:28:22 +0100

    fine margins

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="mt-8">
       <div className="flex bg-cream w-full h-75 items-center justify-center">
         <div className="flex flex-col justify-center items-center text-center">
           <Image src="/Asset3.svg" alt="logo" width={120} height={60}/>
